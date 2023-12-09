76ced4a7cb10713b0e1f3c3bb60b49da9c86cba9
donte5405 <donte5405@outlook.com>
2023-12-10 02:58:57 +0700

    Add custom type

--- addons/chunk/plugin.gd
+++ addons/chunk/plugin.gd
@@ -11,6 +11,8 @@ func _enter_tree() -> void:
 	plugin_panel = load("res://addons/chunk/plugin/plugin_panel.tscn").instance()
 	add_control_to_dock(DOCK_SLOT_LEFT_BL, plugin_panel)
 
+	add_custom_type("EditorChunk", "Spatial", preload("res://addons/chunk/plugin/editor_chunk.gd"), preload("res://addons/chunk/plugin/img/chunk.png"))
+
 
 func _exit_tree() -> void:
 	Engine.remove_meta("chunk_tool_plugin")
